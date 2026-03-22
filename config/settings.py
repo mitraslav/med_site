9d26dc725ff6e45c7d9d1d375ed99fe9d98442c4
slav <vrazumihindima@gmail.com>
2026-03-22 20:01:13 +0300

    templates

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 LOGIN_REDIRECT_URL = 'main:home'
 LOGOUT_REDIRECT_URL = 'main:home'
